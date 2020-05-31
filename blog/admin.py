35b01023372a0621e82f71c8b4d27c2c6bf3e4f7
setcain <charlie.bronson00@gmail.com>
2020-05-31 18:58:06 -0500

    adds models for blog app

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import Category, Post
 
-# Register your models here.
+
+class CategoryAdmin(admin.ModelAdmin):
+    """
+    Read only filds in Admin for categories
+    """
+    readonly_fields = ('created', 'updated')
+
+
+class PostAdmin(admin.ModelAdmin):
+    """
+    Read only fields in admin for Posts
+    """
+    readonly_fields = ('created', 'updated')
+
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Post, PostAdmin)
--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,51 @@
 from django.db import models
+from django.utils.timezone import now
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Category(models.Model):
+    """
+    Category model for blog
+    """
+    name = models.CharField(max_length=100)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        """
+        Meta data in category model
+        """
+        ordering = ['-created']
+        verbose_name_plural = 'categories'
+
+    def __str__(self):
+        """
+        Show category name in admin
+        """
+        return self.name
+
+
+class Post(models.Model):
+    """
+    Post for blog
+    """
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    published = models.DateTimeField(default=now)
+    image = models.ImageField(null=True, blank=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    categories = models.ManyToManyField(Category)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        """
+        Meta data in post model
+        """
+        ordering = ['-created']
+
+    def __str__(self):
+        """
+        Show post title in admin
+        """
+        return self.title
