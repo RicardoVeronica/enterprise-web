90caec47236be70175e3e9e892998208910ea881
setcain <charlie.bronson00@gmail.com>
2020-05-31 11:42:23 -0500

    adds test for static pages

--- pages/tests.py
+++ pages/tests.py
@@ -1,3 +1,27 @@
 from django.test import TestCase
 
-# Create your tests here.
+
+class SimpleTest(TestCase):
+    """
+    Test home, about and timetable pages
+    """
+    def test_home(self):
+        """
+        Test home page estatus code
+        """
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_about(self):
+        """
+        Test about page status code
+        """
+        response = self.client.get('/about/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_timetable(self):
+        """
+        Test timetable page status code
+        """
+        response = self.client.get('/timetable/')
+        self.assertEqual(response.status_code, 200)
