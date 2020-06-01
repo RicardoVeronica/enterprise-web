a8b7cfee541408fe896beac48d1a31dc6688b348
setcain <charlie.bronson00@gmail.com>
2020-06-01 13:20:53 -0500

    updates blog post admin

--- blog/admin.py
+++ blog/admin.py
@@ -14,6 +14,19 @@ class PostAdmin(admin.ModelAdmin):
     Read only fields in admin for Posts
     """
     readonly_fields = ('created', 'updated')
+    list_display = ('title', 'author', 'published', 'post_categories')
+    ordering = ('author', 'published',)
+    search_fields = ('title', 'content', 'author__username',
+                     'categories__name')
+    date_hierarchy = 'published'
+    list_filter = ('author__username', 'categories__name')
+
+    def post_categories(self, obj):
+        """
+        Show categories in list_display
+        """
+        return ", ".join(c.name for c in obj.categories.all().order_by('name'))
+    post_categories.short_description = 'categories'
 
 
 admin.site.register(Category, CategoryAdmin)
